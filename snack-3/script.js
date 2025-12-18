685c62c5f8e8b7499ae577d04c657d2dcdec361c
Luca Striano <luca.striano90@gmail.com>
2025-12-18 16:25:16 +0100

    terzo snack completato

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,17 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const nuovoArray=[];
+
+for (let index = 0; index < numbers.length; index++) {
+
+    const element = numbers[index] + 1;
+
+    nuovoArray.push(element) ;
+
+}
+
+ console.log(nuovoArray);
+
+
