6fc5c45834e81c61ab58c46f003eb62d5f808e11
Luca Striano <luca.striano90@gmail.com>
2025-12-18 16:15:42 +0100

    snack 1 completato

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,8 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// stampo l'array in console
+
+console.log(names);
