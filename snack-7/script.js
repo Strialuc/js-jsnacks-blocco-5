cea4a8c7940b30c9852cc904fbd3ef1a05ba6465
Luca Striano <luca.striano90@gmail.com>
2025-12-18 16:59:08 +0100

    snack 7 completato

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,14 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+for (let index = 0; index < students.length; index++) {
+
+  if (students[index].id === 2) {
+
+      console.log(students[index]);
+      
+  }
+  
+  
+};
