816bdc0bc6bd66acac0e1fe91162b3bbfe83a362
Luca Striano <luca.striano90@gmail.com>
2025-12-18 16:42:25 +0100

    snack 5 completato

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,19 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+
+const numsPari = [];
+
+for (let index = 0; index < nums.length; index++) {
+
+    if (nums[index] % 2 === 0) {
+
+
+        const element = nums[index]
+        numsPari.push(element)
+    }
+
+};
+
+console.log(numsPari);
